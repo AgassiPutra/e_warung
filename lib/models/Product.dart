d18d8d3b2c707c29cca814ae62894d3d6d63418a
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2021-08-30 11:45:25 +0700

    Menambahkan model

---
+++ lib/models/Product.dart
@@ -0,0 +1,24 @@
+class Product{
+  int id;
+  String productName;
+  int sellPrice;
+  int buyPrice;
+  String stock;
+  int status;
+  String imageUrl
+
+  Product({this.id,this.productName,this.sellPrice,this.buyPrice,this.stock,this.status,this.imageUrl});
+
+  Product.fromJson(Map <String, Dynamic> json):
+    id = json['id'],
+    productName = json['product_name'],
+    sellPrice = json['selling_price'],
+    buyPrice = json['purchase_price'],
+    stock = json['stock'],
+    status = json['is_ready'],
+    imageUrl = json['photo']
+  ;
+
+
+
+}
