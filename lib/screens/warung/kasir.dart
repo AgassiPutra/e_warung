cdc846aaa7462602d88fe07ba817335e8ec3ab1c
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2021-09-07 14:56:16 +0700

    Menambahkan slider dan button transaksi halaman kasir

--- lib/screens/warung/kasir.dart
+++ lib/screens/warung/kasir.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
-import "package:sliding_up_panel/sliding_up_panel.dart"
+import "package:sliding_up_panel/sliding_up_panel.dart";
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Kasir extends StatefulWidget{
@@ -10,9 +11,11 @@ class Kasir extends StatefulWidget{
 }
 class _KasirState extends State<Kasir>{
   String dropdownValue = 'Semua';
+  double stateTotal = 0.0;
   
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home:Scaffold(
@@ -37,56 +40,113 @@ class _KasirState extends State<Kasir>{
               ],
           ),
           ),
-          
         ),
-        body: SlidingUpPanel(
-          panel: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children:[
-                Text("0 Barang"),
-                ListView.separated(
-                  itemBuilder: itemBuilder, 
-                  separatorBuilder: separatorBuilder, 
-                  itemCount: itemCount
-                )
-              ]
-            )
-          ),
-          body: Container(
-          padding: const EdgeInsets.all(10),
-          child:Column(
-            children: [
-              DropdownButtonFormField<String>(
-                value: dropdownValue,
-                icon: const Icon(Icons.arrow_drop_down),
-                iconSize: 20,
-                elevation: 16,
-                isExpanded: true,
-                style: const TextStyle(color: Colors.black),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    dropdownValue = newValue!;
-                  });
-                },
-                decoration: InputDecoration(
-                  border:OutlineInputBorder(),
+        body: Stack(
+          children: [
+            SlidingUpPanel(
+              maxHeight: screenSize.height,
+              minHeight: 120,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(15),topRight: Radius.circular(15)),
+              panel: Container(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children:[
+                    Icon(Icons.drag_handle,size: 24,color: Colors.grey,),
+                    Row(mainAxisAlignment:MainAxisAlignment.start,children:[Text("3 Barang",style:TextStyle(fontSize: 18))]),
+                    SizedBox(height:20),
+                    Expanded(child:listRequestProduct()),
+                    // Row(mainAxisAlignment: MainAxisAlignment.spaceAround,children: [Text("Total"), Text("$stateTotal")])
+                  ]
                 ),
-                items: <String>['Semua', 'Daging', 'Sayur', 'Buah'].map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
               ),
-              Expanded(child:listViewProduct())
+              body: Container(
+              padding: const EdgeInsets.all(10),
+              child:Column(
+                children: [
+                  DropdownButtonFormField<String>(
+                    value: dropdownValue,
+                    icon: const Icon(Icons.arrow_drop_down),
+                    iconSize: 20,
+                    elevation: 16,
+                    isExpanded: true,
+                    style: const TextStyle(color: Colors.black),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropdownValue = newValue!;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      border:OutlineInputBorder(),
+                    ),
+                    items: <String>['Semua', 'Daging', 'Sayur', 'Buah'].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                  Expanded(child:listViewProduct())
+                ],
+              )
+              ),
+            ),
+            Positioned(
+              bottom:0,
+              child:Container(
+                decoration: BoxDecoration(color: Colors.white),
+                padding:const EdgeInsets.all(10),
+                width:screenSize.width,
+                height:60,
+                child:ElevatedButton(
+                  onPressed: (){},
+                  child: Text("Transaksi")
+                )
+              )
+            )
           ],
         )
         )
-        )
-      )
-    );
+      );
     
+  }
+  Widget listRequestProduct(){
+    final List<Map> myProducts =
+      List.generate(3, (index) => {"id": index, "name": "Product $index","harga": 3000,"jumlah":index})
+          .toList();
+    
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: myProducts.length,
+      itemBuilder: (BuildContext context, int index) {
+        int subtotal = myProducts[index]['harga'] * myProducts[index]['jumlah'];
+        // double total = 0;
+        // total += (myProducts[index]['harga'] * myProducts[index]['jumlah']);
+        // setState((){
+        //   stateTotal = total;
+        //   }
+        // );
+        return Container(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:[
+              Text(myProducts[index]["name"]),
+              Text("Rp.${myProducts[index]['harga']} x ${myProducts[index]['jumlah']}")
+            ]),
+            Column(
+              children:[
+              Icon(Icons.edit,size: 18,),
+              Text("$subtotal")
+            ]),
+          ],)
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    );
   }
   Widget listViewProduct(){
     final List<String> entries = <String>['A', 'B', 'C', 'D', 'E', 'F', 'G'];
