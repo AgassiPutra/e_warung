ee4118ef538a8a118c934191e08eb79e91c03fef
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2021-09-07 15:15:41 +0700

    Membuat template untuk halaman notifikasi

---
+++ lib/screens/warung/notifikasi.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Notifikasi extends StatefulWidget{
+  const Notifikasi({Key? key}) : super(key: key);
+
+  @override
+  _NotifikasiState createState() => _NotifikasiState();
+}
+class _NotifikasiState extends State<Notifikasi>{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp();
+  }
+}
