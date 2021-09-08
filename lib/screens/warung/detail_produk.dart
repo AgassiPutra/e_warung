96ac450b1e03a5e1db7f87955477eab2245711b4
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2021-09-08 14:49:14 +0700

    Menyiapkan template untuk detail produk

---
+++ lib/screens/warung/detail_produk.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DetailProduk extends StatefulWidget{
+  const DetailProduk({Key? key}) : super(key: key);
+
+  @override
+  _DetailProdukState createState() => _DetailProdukState();
+}
+class _DetailProdukState extends State<DetailProduk>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      
+    );
+  }
+}
