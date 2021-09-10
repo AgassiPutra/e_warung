a5bdabb11268c577802c8439423b57d9ea698aa3
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2021-09-10 14:05:36 +0700

    Menambahkan list view produk

--- lib/screens/warung/produk_warung.dart
+++ lib/screens/warung/produk_warung.dart
@@ -16,10 +16,40 @@ class _WarungState extends State<Warung>{
       body: Container(
         child:Column(
           children: [
-            
+            listViewProduct()
           ]
         )
       )
+    );
+  }
+  Widget listViewProduct(){
+    final List<String> entries = <String>['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: entries.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+            height: 100,
+            child: Card(
+              child: FlatButton(
+                onPressed: () async {
+                  print(index);
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.production_quantity_limits),
+                      title: Text('Produk ${entries[index]}'),
+                      subtitle: Text('Harga '),
+                    ),
+                  ],
+                ),
+              ),
+            ));
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
   }
 }
