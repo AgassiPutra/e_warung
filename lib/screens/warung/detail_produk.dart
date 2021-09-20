ea1bbc890d60d2cbb7e35b9551e3bf526b9cf800
Agassi Putra <agassiputrakhautsar@gmail.com>
2021-09-20 14:31:40 +0700

    memperbaiki button add-minus

--- lib/screens/warung/detail_produk.dart
+++ lib/screens/warung/detail_produk.dart
@@ -1,181 +1,156 @@
 import 'package:flutter/material.dart';
 
-class DetailProduk extends StatefulWidget{
+class DetailProduk extends StatefulWidget {
   const DetailProduk({Key? key}) : super(key: key);
 
   @override
   _DetailProdukState createState() => _DetailProdukState();
 }
-class _DetailProdukState extends State<DetailProduk>{
+
+class _DetailProdukState extends State<DetailProduk> {
   Color _radioBorder = Colors.grey;
   Color _radioText = Colors.black87;
   Color _radioFill = Colors.white;
   int _counter = 0;
   // TextEditingController _textCounter = new TextEditingController();
-  
-  
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset:false,
-      appBar: AppBar(
-        title:Text("Detail Produk"),
-      ),
-      body:Container(
-        // padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(children: [
-              Expanded(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text("Detail Produk"),
+        ),
+        body: Container(
+            // padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Row(children: [
+            Expanded(
                 child: Container(
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color:Colors.pink,
-                  ),
-                  child: Icon(Icons.production_quantity_limits,size:45,color:Colors.white)
-                ))
-              ]
-            ),
-            Container(
-              padding: EdgeInsets.all(15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                Text("Rp.20.000",style:TextStyle(fontWeight: FontWeight.bold,fontSize:30)),
-                SizedBox(height:10),
-                Text("Buah Apel Batu Malang"),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      width:125,
-                      // height:45,
-                      margin: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.pink,
+                    ),
+                    child: Icon(Icons.production_quantity_limits,
+                        size: 45, color: Colors.white)))
+          ]),
+          Container(
+            padding: EdgeInsets.all(15),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text("Rp.20.000",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+              SizedBox(height: 10),
+              Text("Buah Apel Batu Malang"),
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                Container(
+                    width: 125,
+                    // height:45,
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
                         borderRadius: BorderRadius.all(Radius.circular(8)),
-                        border: Border.all(
-                          color:Colors.grey
-                        )
-                      ),
-                      child:Row(
+                        border: Border.all(color: Colors.grey)),
+                    child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
-                        children:[
+                        children: [
                           IconButton(
-                            iconSize:18,
-                            onPressed: (){
-                              setState(() {
-                                if(_counter !=0){
-                                  _counter--;
-                                }
-                              });
-                            },
-                            icon: Icon(Icons.remove,color: ((){if(_counter == 0){return Colors.grey;}else{return Colors.blue;}}()) )
-                          ),
-                          Expanded(child:TextField(
+                              iconSize: 18,
+                              onPressed: () {
+                                setState(() {
+                                  if (_counter != 0) {
+                                    _counter--;
+                                  }
+                                });
+                              },
+                              icon: Icon(Icons.remove,
+                                  color: (() {
+                                    if (_counter == 0) {
+                                      return Colors.grey;
+                                    } else {
+                                      return Colors.blue;
+                                    }
+                                  }()))),
+                          Expanded(
+                              child: TextField(
                             // expands:true,
-                            controller: TextEditingController()..text = '$_counter',
-                            onChanged: (text){
+                            controller: TextEditingController()
+                              ..text = '$_counter',
+                            onChanged: (text) {
                               _counter = int.parse(text);
                               text = "$_counter";
                             },
                             textAlignVertical: TextAlignVertical.center,
-                            decoration: InputDecoration(
-                              border: InputBorder.none
-                            ),
+                            decoration:
+                                InputDecoration(border: InputBorder.none),
                             keyboardType: TextInputType.number,
                           )),
                           IconButton(
-                            iconSize:18,
-                            onPressed: (){
-                              setState(() {
+                              iconSize: 18,
+                              onPressed: () {
+                                setState(() {
                                   _counter++;
-                              });
-                            },
-                            icon: Icon(Icons.add,color: Colors.blue)
+                                });
+                              },
+                              icon: Icon(Icons.add, color: Colors.blue)),
+                        ]))
+              ])
+            ]),
+          ),
+          Divider(thickness: 4),
+          Container(
+              padding: EdgeInsets.all(15),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Pilih Satuan:"),
+                    Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    color: _radioFill,
+                                    border: Border.all(color: _radioBorder)),
+                                child: Text("Pcs",
+                                    style: TextStyle(color: _radioText))),
                           ),
-                        ]
-                      )
-                    )
-                  ]
-                )
-              ]),
-            ),
-            Divider(thickness: 4),
-            Container(
-              padding:EdgeInsets.all(15),
-              child:Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Pilih Satuan:"),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                    GestureDetector(
-                      onTap:(){
-                        
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: _radioFill,
-                          border: Border.all(
-                            color: _radioBorder
-                          )
-                        ),
-                        child: Text("Pcs",style: TextStyle(color:_radioText))
-                      ),
-                    ),
-                    SizedBox(width:15),
-                    GestureDetector(
-                      onTap:(){
+                          SizedBox(width: 15),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    color: _radioFill,
+                                    border: Border.all(color: _radioBorder)),
+                                child: Text("Paket",
+                                    style: TextStyle(color: _radioText))),
+                          ),
+                          SizedBox(width: 15),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    color: _radioFill,
+                                    border: Border.all(color: _radioBorder)),
+                                child: Text("Kg",
+                                    style: TextStyle(color: _radioText))),
+                          ),
+                        ])
+                  ])),
 
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: _radioFill,
-                          border: Border.all(
-                            color: _radioBorder
-                          )
-                        ),
-                        child: Text("Paket",style: TextStyle(color:_radioText))
-                      ),
-                    ),
-                    SizedBox(width:15),
-                    GestureDetector(
-                      onTap:(){
-                        
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: _radioFill,
-                          border: Border.all(
-                            color: _radioBorder
-                          )
-                        ),
-                        child: Text("Kg",style: TextStyle(color:_radioText))
-                      ),
-                    ),
-                    
-                  ])
-                ]
-              )
-            ),
-            
-            // // Deskripsi Produk
-            // Text("Deskripsi Produk",style:TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
-            // SizedBox(height: 15,),
-            // Text('''
-            //   Apel mengandung flavonoids, fruktosa dan serat. Jika dihitung dalam 100 g buah apel, terdapat kandungan serta sebesar 2,1 g. Bila kulitnya kita kupas, kandungan serat tersebut masih tetap tinggi.
-              
-            //   Pada kulit buah apel yang telah terkupas, kandungan serat yang tersisa berkisar 1,9 g. Menurut penelitian, serat buah ini mampu menurunkan kadar kolesterol darah dan resiko penyakit jantung koroner.
-            //   ''',maxLines: 20, softWrap: true)
-          ]
-        )
-      )
-    );
+          // // Deskripsi Produk
+          // Text("Deskripsi Produk",style:TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
+          // SizedBox(height: 15,),
+          // Text('''
+          //   Apel mengandung flavonoids, fruktosa dan serat. Jika dihitung dalam 100 g buah apel, terdapat kandungan serta sebesar 2,1 g. Bila kulitnya kita kupas, kandungan serat tersebut masih tetap tinggi.
+
+          //   Pada kulit buah apel yang telah terkupas, kandungan serat yang tersisa berkisar 1,9 g. Menurut penelitian, serat buah ini mampu menurunkan kadar kolesterol darah dan resiko penyakit jantung koroner.
+          //   ''',maxLines: 20, softWrap: true)
+        ])));
   }
 }
--- lib/screens/warung/keranjang.dart
+++ lib/screens/warung/keranjang.dart
@@ -1,117 +1,123 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-class Keranjang extends StatefulWidget{
-  
+
+class Keranjang extends StatefulWidget {
   @override
   _KeranjangState createState() => _KeranjangState();
 }
-class _KeranjangState extends State<Keranjang>{
-  
-  Widget build(BuildContext context){
+
+class _KeranjangState extends State<Keranjang> {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Keranjang"),
       ),
       body: Container(
-        padding: EdgeInsets.all(10),
-        child:Column(
-          children: [
-            Expanded(child: listKeranjang()),
-            Row(
-              children:[
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Expanded(child: listKeranjang()),
+              Row(children: [
                 Expanded(
-                  child:ElevatedButton(
-                    onPressed: (){}, 
-                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.green)),
-                    child: Text("Pesan Sekarang")
-                  )
-                )
-              ]
-            )
-          ],
-        )
-      ),
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.green)),
+                        child: Text("Pesan Sekarang")))
+              ])
+            ],
+          )),
     );
   }
-  Widget listKeranjang(){
-    Random rand = new Random();
-    
-    List<Map> keranjangku = List.generate(4, (index) => {
-      "id": index,
-      "product_name": "Product $index",
-      "harga": "${index+1}.000",
-      "stok": 8
-    }); 
+
+  Widget listKeranjang() {
+    List<Map> keranjangku = List.generate(
+        4,
+        (index) => {
+              "id": index,
+              "product_name": "Product $index",
+              "harga": "${index + 1}.000",
+              "stok": 5
+            });
 
     return ListView.separated(
       itemCount: keranjangku.length,
-      itemBuilder: (BuildContext context, int index){
+      itemBuilder: (BuildContext context, int index) {
         return Card(
-          child: ListTile(
-            leading: Container(
-              height:150,
-              decoration: BoxDecoration(color:Colors.yellow.shade400),
-              child: Icon(Icons.check_box_outline_blank_rounded)
-            ),
-            title: Text(keranjangku[index]['product_name']),
-            subtitle:Row(
-              children:[
-                Text(keranjangku[index]['harga']),
-                IconButton(onPressed: (){},icon: Icon(Icons.delete)),
-                buttonAddMinus(counter: keranjangku[index]['stok'],max:10)
-              ]
-            )
-          )
-        );
-      }, 
-      separatorBuilder: (BuildContext context, int index) => Divider(), 
+            child: Container(
+                padding: EdgeInsets.all(8),
+                alignment: Alignment.center,
+                child: ListTile(
+                    leading: Icon(Icons.shopping_cart),
+                    title: Text(keranjangku[index]['product_name']),
+                    subtitle: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(keranjangku[index]['harga']),
+                          Row(children: [
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.delete, color: Colors.red)),
+                            buttonAddMinus(
+                                counter: keranjangku[index]['stok'], max: 10)
+                          ])
+                        ]))));
+      },
+      separatorBuilder: (BuildContext context, int index) => Divider(),
     );
   }
-  Widget buttonAddMinus({required int counter,int? max}){
-    int _counter = counter;
+
+  Widget buttonAddMinus({required int counter, int? max}) {
+    int _counter = 0;
     return Container(
-      width: 130,
-      height: 60,
-      child: Row(
-        children:[
+        width: 130,
+        height: 60,
+        child: Row(children: [
           IconButton(
-            onPressed: (){
-              setState(() {
-                if(_counter !=0){
-                  _counter--;
-                }
-              });
-            },
-            icon: Icon(Icons.remove,color: ((){if(counter == 0){return Colors.grey;}else{return Colors.blue;}}()))
-          ),
-          Expanded(
-            child:TextField(
-              // expands:true,
-              textAlign:TextAlign.center,
-              controller: TextEditingController()..text = '$_counter',
-              onChanged: (text){
-                counter = int.parse(text);
-                text = "$_counter";
+              onPressed: () {
+                setState(() {
+                  if (counter != 0) {
+                    _counter = counter - 1;
+                  }
+                });
               },
-              textAlignVertical: TextAlignVertical.center,
-              decoration: InputDecoration(
-                border: InputBorder.none
-              ),      
-              keyboardType: TextInputType.number,
-            )
-          ),
-          IconButton(
-            onPressed: (){
-              setState(() {
-                if(_counter != max){
-                  _counter++;
-                }
-              });
+              icon: Icon(Icons.remove,
+                  color: (() {
+                    if (counter == 0) {
+                      return Colors.grey;
+                    } else {
+                      return Colors.blue;
+                    }
+                  }()))),
+          Expanded(
+              child: TextField(
+            // expands:true,
+            textAlign: TextAlign.center,
+            controller: TextEditingController()..text = '$counter',
+            onChanged: (text) {
+              counter = int.parse(text);
+              text = "$   counter";
             },
-            icon: Icon(Icons.add,color: ((){if(_counter == max){return Colors.grey;}else{return Colors.blue;}}()))
-          ),
-        ]
-      )
-    );
+            textAlignVertical: TextAlignVertical.center,
+            decoration: InputDecoration(border: InputBorder.none),
+            keyboardType: TextInputType.number,
+          )),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  if (counter != max) {
+                    counter++;
+                  }
+                });
+              },
+              icon: Icon(Icons.add,
+                  color: (() {
+                    if (counter == max) {
+                      return Colors.grey;
+                    } else {
+                      return Colors.blue;
+                    }
+                  }()))),
+        ]));
   }
 }
