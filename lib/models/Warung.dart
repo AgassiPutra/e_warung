f0007e99c351628ee9daf7a7518c5f7ab77f226f
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2021-10-05 13:45:46 +0700

    Menghapus file yang tidak digunakan

--- lib/models/Warung.dart
+++
@@ -1,9 +0,0 @@
-// class Warung{
-//   int id;
-//   String namaWarung;
-//   String productName;
-//   String stock;
-//   String date;
-
-//   Warung({this.id,this.namaWarung,this.productName,this.stock,this.date});s
-// }
